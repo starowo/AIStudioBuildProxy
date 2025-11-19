c5d6d32698a62553816ba7ab46eb90bdd2c302a9
starowo <87891461+starowo@users.noreply.github.com>
2025-11-18 19:39:26 -0800

    feat: satellite

--- index.js
+++ index.js
@@ -1,22 +1,43 @@
 // AI Studio Proxy Extension
 // Entry point
+import { proxies } from '../../../openai.js';
+import { extension_settings, getContext, renderExtensionTemplateAsync } from '../../../extensions.js';
 
 const extensionName = "AIStudioBuildProxy";
-
+let satellite;
 jQuery(async () => {
     try {
+        const html = await renderExtensionTemplateAsync('third-party/AIStudioBuildProxy', 'index');
+        $("#extensions_settings2").append(html);
         const SillyTavern = window.SillyTavern;
         if (!SillyTavern) {
             console.error(`${extensionName}: SillyTavern global object not found.`);
             return;
         }
 
+        // check for proxy
+        if (!proxies.find(proxy => proxy.name == "AIStudio反代")) {
+            proxies.push({
+                name: "AIStudio反代",
+                url: "http://127.0.0.1:8889",
+                password: ""
+            });
+            const option = document.createElement('option');
+            option.text = String("AIStudio反代");
+            option.value = String("AIStudio反代");
+
+            $('#openai_proxy_preset').append(option);
+        }
+
         const context = SillyTavern.getContext();
         console.log(`${extensionName} extension loaded.`);
 
-        // You can access context.chat, context.characters, etc. here
-        // console.log("Current character:", context.characters[context.characterId]);
-
+        $("#aistudio_proxy_button").on("click", () => {
+            if (satellite) {
+                satellite.close();
+            }
+            satellite = window.open("https://ai.studio/apps/drive/1V2aNY3lZbtLZ58YQOfFnVg82uNikWqlM?fullscreenApplet=true", "satellite", "width=1280,height=720");
+        });
     } catch (error) {
         console.error(`${extensionName} failed to load:`, error);
     }
